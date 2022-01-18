45b1acc6f2bc6be85873f091cb157a287fae1710
Mohammed Taie <mza004@live.aul.edu.lb>
2022-01-18 13:51:44 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, customize, timeseries, data_collection,\
-vegetation_analysis , basemap, supervised_learning, timeseries
+from apps import home, timeseries, \
+vegetation_analysis , basemap, supervised_learning
 
 st.set_page_config(layout="wide")
 
@@ -15,9 +15,6 @@ apps.add_app("Vegetation analysis", vegetation_analysis.app)
 apps.add_app("Supervised learning", supervised_learning.app)
 apps.add_app("Time series analysis", timeseries.app)
 
-
-# apps.add_app("Customize basemap", customize.app)
-# apps.add_app("Data collection", data_collection.app)
 
 # The main app
 apps.run()
